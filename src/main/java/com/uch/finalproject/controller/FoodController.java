7a22906d6edc3fc8ed891b1f22422b2a28c79965
ncu2023 <aaronbeango@gmail.com>
2023-07-09 14:55:46 +0800

    Update foods

--- src/main/java/com/uch/finalproject/controller/FoodController.java
+++ src/main/java/com/uch/finalproject/controller/FoodController.java
@@ -1,9 +1,11 @@
 package com.uch.finalproject.controller;
 
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,6 +23,33 @@ public class FoodController {
     @RequestMapping(value = "/foods", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public FoodResponse foods() {
         return getFoodList();
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public FoodResponse add() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/shopping?user=root&password=0000");
+
+            stmt = conn.prepareStatement("INSERT INTO foods VALUES(null,?,?,?, ?)");
+            stmt.setInt(1, 1);
+            stmt.setDate(2, java.sql.Date.valueOf("2013-09-04"));
+            stmt.setDate(3, java.sql.Date.valueOf("Sun Jul 16 2023 00:00:00 GMT+0800 (\u53F0\u5317\u6A19\u6E96\u6642\u9593)"));
+            stmt.setInt(4, 999);
+
+            stmt.executeUpdate();
+
+            return new FoodResponse(0, "成功", null);
+        } catch(SQLException e) {
+            return new FoodResponse(e.getErrorCode(), e.getMessage(), null);
+        } catch(ClassNotFoundException e) {
+            return new FoodResponse(1, "無法註冊驅動程式", null);
+        }
     }
 
     private FoodResponse getFoodList() {
