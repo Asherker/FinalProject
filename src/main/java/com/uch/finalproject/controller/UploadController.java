9afa574bf882d09763e782fde50cad072300d024
ncu2023 <aaronbeango@gmail.com>
2023-07-11 15:23:03 +0800

    常數抽離

--- src/main/java/com/uch/finalproject/controller/UploadController.java
+++ src/main/java/com/uch/finalproject/controller/UploadController.java
@@ -2,6 +2,8 @@ package com.uch.finalproject.controller;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +11,25 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.uch.finalproject.model.UploadResponse;
 
+import lombok.Data;
+
 @RestController
 public class UploadController {
+    @Value("${upload.url.base-path}")
+    private String urlBasePath;
+
+    @Value("${upload.server.path}")
+    private String serverUploadPath;
+
     @PostMapping("/file")
     public UploadResponse uploadFileMulti(@RequestParam("files") MultipartFile[] uploadfiles) {
-
+    
         String fileName = uploadfiles[0].getOriginalFilename();
         try {
-            uploadfiles[0].transferTo(new File(new File("src/main/resources/static/imgs/").getAbsolutePath() + "/" + fileName));
+            uploadfiles[0].transferTo(new File(new File(serverUploadPath).getAbsolutePath() + "/" + fileName));
         } catch (Exception e) {
             return new UploadResponse(1, e.toString(), null);
         }
-        return new UploadResponse(0, "成功上傳", "/imgs/" + fileName);
+        return new UploadResponse(0, "成功上傳", urlBasePath + fileName);
     }
 }
